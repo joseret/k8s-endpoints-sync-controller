ed581b62b07d974347cdbfa61606122a6ebb8480
Jose Retelny <joseret@google.com>
2023-04-04 20:28:32 +0000

    get synching

---
+++ scripts/jr-build.sh
@@ -0,0 +1,5 @@
+git commit -am "get synching" && git push
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -a -o ./dist/k8s-endpoints-sync-controller src/main/main.go
+
+docker build -t  gcr.io/jr-network-infra-1-4978/joseret/k8s-endpoints-sync-controller:latest  .
+docker push  gcr.io/jr-network-infra-1-4978/joseret/k8s-endpoints-sync-controller:latest
